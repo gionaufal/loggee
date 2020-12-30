b7d78a32bffbf1954334096f1575988d008986d8
Giovanni Filippetti <giovanni.filippetti@suse.com>
2020-12-30 15:48:25 +0100

    Add get game

--- lib/bgg/client.ex
+++ lib/bgg/client.ex
@@ -1,5 +1,6 @@
 defmodule Loggee.Bgg.Client do
   use Tesla
+  import SweetXml
 
   plug Tesla.Middleware.BaseUrl, "https://boardgamegeek.com/xmlapi2"
 
@@ -7,14 +8,20 @@ defmodule Loggee.Bgg.Client do
     "/search?query=#{name}&type=boardgame"
     |> get()
     |> handle_get()
-    |> organize_payload()
+    |> organize_search_payload()
+  end
+
+  def game(id) do
+    "/thing?id=#{id}&stats=1"
+    |> get()
+    |> organize_game_payload()
   end
 
   defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}) do
     {:ok, XmlToMap.naive_map(body)}
   end
 
-  defp organize_payload({:ok, payload}) do
+  defp organize_search_payload({:ok, payload}) do
     items = payload["items"]
     games = items["#content"]["item"] |> Enum.map(fn game ->
       %{
@@ -24,5 +31,26 @@ defmodule Loggee.Bgg.Client do
       }
     end)
     {:ok, %{games: games, total: items["-total"]}}
+  end
+
+  defp organize_game_payload({:ok, %Tesla.Env{body: body}}) do
+    body |> xmap(
+      name: ~x"//name[@type='primary']/@value",
+      description: ~x"//description/text()",
+      year_published: ~x"//yearpublished/@value",
+      id: ~x"//item/@id",
+      rating: ~x"//statistics/ratings/average/@value",
+      weight: ~x"//statistics/ratings/averageweight/@value",
+      suggested_players: [
+        ~x"//poll[@name='suggested_numplayers']/results"l,
+        value: ~x"./@numplayers",
+        best: ~x".//result[@value='Best']/@numvotes",
+        recommended:  ~x".//result[@value='Recommended']/@numvotes"
+      ],
+      language_dependence: [
+        ~x"//poll[@name='language_dependence']//result"l,
+        value: ~x"./@value",
+        votes: ~x"./@numvotes",
+      ])
   end
 end
